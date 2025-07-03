55e8ccfc09633fab22f64164a1759fbc2785468d
kirtirajsinh <kirtiraj3738@gmail.com>
2025-07-03 18:14:23 +0000

    Add image remote patterns configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        pathname: "**",
+      },
+    ],
+  },
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
 };
 
 export default nextConfig;
